5cdce33e25d3cdf61cda5f2a9e989bef3cfe8280
Dan <dan12411@fugle.tw>
2023-05-22 21:06:24 +0800

    feat: add isEnabled variable

--- DesignSystem/PrimaryButtonStyle.swift
+++ DesignSystem/PrimaryButtonStyle.swift
@@ -1,16 +1,18 @@
 import SwiftUI
 
 public struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
     public init() {}
 
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(width: 112, height: 36)
-            .foregroundColor(configuration.isPressed ? Asset.primary.swiftUIColor : Asset.onPrimary.swiftUIColor)
+            .foregroundColor(!isEnabled ? Asset.onPrimary.swiftUIColor : (configuration.isPressed ? Asset.primary.swiftUIColor : Asset.onPrimary.swiftUIColor))
             .font(Font.body)
             .padding(10)
             .background(
-                configuration.isPressed ? Asset.onPrimary.swiftUIColor : Asset.primary.swiftUIColor
+                !isEnabled ? Asset.primary.swiftUIColor.opacity(0.5) : (configuration.isPressed ? Asset.onPrimary.swiftUIColor : Asset.primary.swiftUIColor)
             )
             .cornerRadius(100)
     }
