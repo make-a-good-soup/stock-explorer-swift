6a259531c4254efd26a64f3ee3b47e8fcb15647d
Dan <dan12411@fugle.tw>
2023-05-30 20:06:00 +0800

    fix: primary button style pressed color.

--- DesignSystem/PrimaryButtonStyle.swift
+++ DesignSystem/PrimaryButtonStyle.swift
@@ -8,11 +8,11 @@ public struct PrimaryButtonStyle: ButtonStyle {
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(width: 112, height: 36)
-            .foregroundColor(!isEnabled ? Asset.onPrimary.swiftUIColor : (configuration.isPressed ? Asset.primary.swiftUIColor : Asset.onPrimary.swiftUIColor))
+            .foregroundColor(!isEnabled ? Asset.onPrimary.swiftUIColor : Asset.onPrimary.swiftUIColor)
             .font(Font.body)
             .padding(10)
             .background(
-                !isEnabled ? Asset.primary.swiftUIColor.opacity(0.5) : (configuration.isPressed ? Asset.onPrimary.swiftUIColor : Asset.primary.swiftUIColor)
+                !isEnabled ? Asset.primary.swiftUIColor.opacity(0.5) : (configuration.isPressed ? Asset.onPrimaryContainer.swiftUIColor : Asset.primary.swiftUIColor)
             )
             .cornerRadius(100)
     }
