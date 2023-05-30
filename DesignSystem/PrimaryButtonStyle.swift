18adceba3845b820fbfdd02a8b69dfce18d098ac
TeChengHung <dan12411@gmail.com>
2023-05-30 21:47:29 +0800

    [Added] Design System  (#2)
    * feat: add `Design System` module.
    
    * feat: import `DesignSystem`.
    
    * feat: add isEnabled variable
    
    * fix: primary button style pressed color.
    
    ---------
    
    Co-authored-by: Dan <dan12411@fugle.tw>

---
+++ DesignSystem/PrimaryButtonStyle.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+public struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
+    public init() {}
+
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 112, height: 36)
+            .foregroundColor(!isEnabled ? Asset.onPrimary.swiftUIColor : Asset.onPrimary.swiftUIColor)
+            .font(Font.body)
+            .padding(10)
+            .background(
+                !isEnabled ? Asset.primary.swiftUIColor.opacity(0.5) : (configuration.isPressed ? Asset.onPrimaryContainer.swiftUIColor : Asset.primary.swiftUIColor)
+            )
+            .cornerRadius(100)
+    }
+}
